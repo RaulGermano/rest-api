6d4367d9b767a0faa5f1220feaf474a092ff8477
RaulGermano <raul.germano@icloud.com>
2019-09-06 14:10:21 -0300

    updated

--- src/controllers/clientVehicle.js
+++ src/controllers/clientVehicle.js
@@ -6,40 +6,24 @@ module.exports = {
 	async CreateClientVehicle(req, res) {
 		const { plate, name, client_id } = req.body;
 
-		const client = await Client.findOne({
-			_id: client_id
-		});
-
-		// client.update(
-		// 	{ 'vehicle.plate': 'admin' },
-		// 	{ $addToSet: { 'vehicle.plate': plate } }
-		// );
-
-		const teste = await Client.updateOne(
-			{ _id: client_id, 'vehicle.plate': { $ne: plate } },
-			{
-				$push: {
-					vehicle: { plate, name }
+		try {
+			const result = await Client.updateOne(
+				{ _id: client_id, 'vehicle.plate': { $ne: plate } },
+				{
+					$push: {
+						vehicle: { plate, name }
+					}
 				}
-			}
-		);
+			);
 
-		// client.update(
-		// 	{ 'vehicle.plate': { $ne: plate } },
-		// 	{ $push: { vehicle: plate } },
-		// 	err => {
-		// 		console.log(err);
-		// 	}
-		// );
-
-		// client.vehicle.push({
-		// 	plate,
-		// 	name
-		// });
-
-		// await teste.save();
-
-		return res.json(client);
+			return res.json({
+				message: result
+			});
+		} catch (error) {
+			return res.status(400).json({
+				error
+			});
+		}
 	},
 
 	///////////////////////////////////////////////  selects
